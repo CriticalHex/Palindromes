3d0707648e1b8d0bd49baf41b069746eb1d3ee25
Email 6 <loserkid0006@outlook.com>
2022-02-28 14:57:03 -0700

    Math and String for Palindromes

---
+++ Source.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<string>
+#include<cmath>
+using namespace std;
+
+bool palindrome(int x) {
+	string test = to_string(x);
+
+	for (int i = 0; i < test.length(); i++) {
+		if (test[i] != test[test.length() - i - 1]) return false;
+	}
+	return true;
+}
+
+bool intPalindrome(int a) {
+	if (a < 0) {
+		return false;
+	}
+	else if (a < 10) {
+		return true;
+	}
+	int n = (floor(log10(a)) + 1);
+	int inverse = 0;
+	for (int x = 0; x <= n - 1; x++) {
+		inverse += (((floor(a / pow(10, x))) - (floor((floor(a / pow(10, x))) / 10)) * 10) * (pow(10, n - x - 1)));
+	}
+	if (a == inverse)
+		return true;
+	return false;
+}
+
+int main() {
+	//cout << palindrome(102);
+	cout << intPalindrome(101) << endl;
+}
